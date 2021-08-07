4932e1cfbaeec197093ad5f5fae59e8a45b52bbc
0xEquinox <WilliamJMcdonald@protonmail.com>
2021-08-07 16:08:40 -0700

    added print

--- src/main.rs
+++ src/main.rs
@@ -13,36 +13,77 @@ fn main() {
         let line = line.split(" ");
         let line: Vec<&str> = line.collect();
 
-        for i in line{
-            if i.contains("+"){
+        for mut i in 0..line.len() {
+            if line[i].contains("+"){
                 let ans = add(stack[stack.len() - 1], stack[stack.len() - 2]);
                 stack.pop();
                 stack.pop();
                 stack.push(ans);
-            }else if i.contains("-"){
+            }else if line[i].contains("-"){
                 let ans = subtract(stack[stack.len() - 2], stack[stack.len() - 1]);
                 stack.pop();
                 stack.pop();
                 stack.push(ans);
-            }else if i.contains("*"){
+            }else if line[i].contains("*"){
                 let ans = mulitply(stack[stack.len() - 2], stack[stack.len() - 1]);
                 stack.pop();
                 stack.pop();
                 stack.push(ans);
-            }else if i.contains("/"){
+            }else if line[i].contains("/"){
                 let ans = divide(stack[stack.len() - 2], stack[stack.len() - 1]);
                 stack.pop();
                 stack.pop();
                 stack.push(ans);
-            }else if i.contains(".s"){
+            }else if line[i].contains(".s"){
                 println!("{:?}", stack);
-            }else if i.contains(">>"){
+            }else if line[i].contains(">>"){
                 println!("{}", stack[stack.len() - 1]);
+                stack.pop();
+            }else if line[i].contains("."){
+                stack.pop();
+            }else if line[i].contains("="){
+                let ans = equals(stack[stack.len() - 1], stack[stack.len() - 2]);
+                stack.pop();
+                stack.pop();
+                stack.push(ans);
+            }else if line[i].contains("if"){
+                let ans = if_statments(stack[stack.len() - 1] as i32, &line, i as i32);
+                if ans != 0{
+                    i = ans as usize;
+                }
+            }else if line[i].contains("then"){
+                continue;
             }else{
-                let num: i32 = i.trim().parse().unwrap();
+                let num: i32 = line[i].trim().parse().unwrap();
                 stack.push(num);
             }
+            println!("{}", i)
         }
+    }
+}
+
+fn if_statments(enumerator: i32, line: &Vec<&str>, current_index: i32) -> i32{
+    if enumerator == 1{
+        return 0;
+    }else{
+        let  i = current_index;
+        let mut count = 0;
+        for i in 0..line.len(){
+            if line[i] == "then" {
+                return count;
+            }
+            count += 1;
+        }
+        println!("Error");
+    }
+    return 0;
+}
+
+fn equals(num1: i32, num2: i32) -> i32{
+    if num1 == num2 {
+        return 1;
+    }else{
+        return 0;
     }
 }
 

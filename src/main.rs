4ef7067f84b4651094bfd709bd35601293d6e3da
DevEquinox <69062137+DevEquinox@users.noreply.github.com>
2021-08-07 12:37:53 -0700

    Added shit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use std::io;
+
+
+fn main() {
+
+    println!("Equinox Lang v0.1");
+
+    let mut running = true;
+    let mut stack:Vec<i32> = Vec::new();
+
+    while running {
+        let line = console_input();
+        let line = line.split(" ");
+        let line: Vec<&str> = line.collect();
+
+        for i in line{
+            if i.contains("+"){
+                let ans = add(stack[stack.len() - 1], stack[stack.len() - 2]);
+                stack.pop();
+                stack.pop();
+                stack.push(ans);
+            }else if i.contains("-"){
+                let ans = subtract(stack[stack.len() - 2], stack[stack.len() - 1]);
+                stack.pop();
+                stack.pop();
+                stack.push(ans);
+            }else if i.contains("*"){
+                let ans = mulitply(stack[stack.len() - 2], stack[stack.len() - 1]);
+                stack.pop();
+                stack.pop();
+                stack.push(ans);
+            }else if i.contains("/"){
+                let ans = divide(stack[stack.len() - 2], stack[stack.len() - 1]);
+                stack.pop();
+                stack.pop();
+                stack.push(ans);
+            }else if i.contains(".s"){
+                println!("{:?}", stack);
+            }else{
+                let num: i32 = i.trim().parse().unwrap();
+                stack.push(num);
+            }
+        }
+    }
+}
+
+fn add(num1: i32, num2: i32) -> i32{
+    return num1 + num2;
+}
+
+fn subtract(num1: i32, num2: i32) -> i32{
+    return num1 - num2;
+}
+
+fn mulitply(num1: i32, num2: i32) -> i32{
+    return num1 * num2;
+}
+
+fn divide(num1: i32, num2: i32) -> i32{
+    return num1 / num2;
+}
+
+fn console_input() -> String{
+
+    let mut input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut input);
+
+    return input;
+}
